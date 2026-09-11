46e4d66440f46acf89dc2ded39151499049a7c81
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-11 12:15:48 -0500

    Initial working WhatsUpShop baseline

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$tableprefix = "cds_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
+$databasename = "tienda_cds_reggaeton";
+$dbuser = "root";
+$dbpassword = "";

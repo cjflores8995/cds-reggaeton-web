3f837d0778dba0073fc12b2c5879e36289f38ec0
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-12 15:23:50 -0500

    Load storefront UI enhancements

--- store-footer.php
+++ store-footer.php
@@ -239,6 +239,10 @@ if(isset($storeBaseUrl) && trim((string)$storeBaseUrl) !== ""){
     ></script>
     <script
         defer
+        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>store-ui-enhancements.js?v=1"
+    ></script>
+    <script
+        defer
         src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>analytics-client.js?v=2"
         data-endpoint="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>analytics-event.php"
         data-timeout-ms="1800"

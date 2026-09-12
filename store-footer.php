8d67106bd0df594ee9c72211171e0deb7416384f
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-12 00:24:47 -0500

    Use fail-open analytics client on storefront

--- store-footer.php
+++ store-footer.php
@@ -235,8 +235,9 @@ if(isset($storeBaseUrl) && trim((string)$storeBaseUrl) !== ""){
 <?php if($footerAssetBaseUrl !== ""){ ?>
     <script
         defer
-        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>analytics-client.js?v=1"
+        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>analytics-client.js?v=2"
         data-endpoint="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>analytics-event.php"
+        data-timeout-ms="1800"
     ></script>
     <script
         defer

34417447d51d39cde4692388a8c12587a444aa67
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-14 16:29:13 -0500

    Load Sales Studio lot cover preview

--- admin-menu.php
+++ admin-menu.php
@@ -291,4 +291,8 @@ if(
         defer
         src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-templates.js?v=3", ENT_QUOTES, "UTF-8"); ?>"
     ></script>
+    <script
+        defer
+        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-lot.js?v=1", ENT_QUOTES, "UTF-8"); ?>"
+    ></script>
 <?php } ?>

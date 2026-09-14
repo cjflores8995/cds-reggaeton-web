3ba00a01f8e410d6869485a30e2340ce4702d77a
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-13 20:02:33 -0500

    Bust compact admin widget loader cache

--- store-footer.php
+++ store-footer.php
@@ -243,7 +243,7 @@ if(isset($storeBaseUrl) && trim((string)$storeBaseUrl) !== ""){
 <?php if($footerAssetBaseUrl !== ""){ ?>
     <script
         defer
-        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>store-branding.js?v=3"
+        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>store-branding.js?v=4"
     ></script>
     <script
         defer

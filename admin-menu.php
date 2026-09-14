965d22b305f35a8ff7a1f8ef4330487ea32544bd
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-14 18:55:59 -0500

    Bust one-click Sales Studio downloader

--- admin-menu.php
+++ admin-menu.php
@@ -305,6 +305,6 @@ if(
     ></script>
     <script
         defer
-        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-export-direct.js?v=1", ENT_QUOTES, "UTF-8"); ?>"
+        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-export-direct.js?v=2", ENT_QUOTES, "UTF-8"); ?>"
     ></script>
 <?php } ?>

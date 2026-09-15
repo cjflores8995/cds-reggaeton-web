948c9ce0b4d479ac999e95b0317a2baae7b3a350
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-14 19:42:11 -0500

    Load Sales Studio flow cleanup

--- admin-menu.php
+++ admin-menu.php
@@ -311,4 +311,8 @@ if(
         defer
         src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-marketplace-copy.js?v=2", ENT_QUOTES, "UTF-8"); ?>"
     ></script>
+    <script
+        defer
+        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-flow-ui.js?v=1", ENT_QUOTES, "UTF-8"); ?>"
+    ></script>
 <?php } ?>

da4c54a81bb2096162be813cd8eba384ed7f7d1a
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-14 19:27:12 -0500

    Bust smart Marketplace copy cache

--- admin-menu.php
+++ admin-menu.php
@@ -309,6 +309,6 @@ if(
     ></script>
     <script
         defer
-        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-marketplace-copy.js?v=1", ENT_QUOTES, "UTF-8"); ?>"
+        src="<?php echo htmlspecialchars($baseurl . "admin-sales-studio-marketplace-copy.js?v=2", ENT_QUOTES, "UTF-8"); ?>"
     ></script>
 <?php } ?>

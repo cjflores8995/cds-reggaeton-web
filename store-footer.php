0a3aad38cb2c0485ee3ecfab8eaa0d64bca58f8d
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-18 17:09:27 -0500

    Bust shipping scope cache after header fix

--- store-footer.php
+++ store-footer.php
@@ -387,7 +387,7 @@ require __DIR__ . "/artist-collection-runtime.php";
     ></script>
     <script
         defer
-        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>store-shipping-scope.js?v=1"
+        src="<?php echo storeFooterEsc($footerAssetBaseUrl); ?>store-shipping-scope.js?v=2"
     ></script>
     <script
         defer

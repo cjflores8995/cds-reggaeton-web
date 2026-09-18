b9e18dd5af25514537171f612cda8f87eb696957
Carlos Flores <carlos_jhousef593@hotmail.com>
2026-09-18 17:56:53 -0500

    Add reggaeton CDs landing to sitemap

--- sitemap.php
+++ sitemap.php
@@ -147,6 +147,10 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
         <loc><?php echo sitemapXml(seoUrl("envios-y-devoluciones")); ?></loc>
     </url>
 
+    <url>
+        <loc><?php echo sitemapXml(seoUrl("cds-reggaeton")); ?></loc>
+    </url>
+
     <?php foreach($artists as $artist){ ?>
         <url>
             <loc><?php echo sitemapXml(
